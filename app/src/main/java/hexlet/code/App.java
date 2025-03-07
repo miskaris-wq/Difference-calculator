b84bb489cc1e98e7106bcb85de4ed19696edf26c
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2025-03-07 13:59:57 +0300

    step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = false,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.",
+        synopsisHeading = "Usage: ",
+        customSynopsis = "gendiff [-hV]"
+)
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean help;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean version;
+
+    @Override
+    public void run() {
+        System.out.println("Gendiff is running...");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
