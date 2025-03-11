91380426cca0dfc2a0dedcc1a199c6057014beef
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2025-03-11 23:25:06 +0300

    step 7

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
@@ -12,11 +11,7 @@ class DifferTest {
         String path1 = "/Users/rasskazovaksenya/java-project-71/app/src/test/resources/test1.json";
         String path2 = "/Users/rasskazovaksenya/java-project-71/app/src/test/resources/test2.json";
 
-        String expected = "{\n" +
-                "    host: hexlet.io\n" +
-                "    timeout: 20\n" +
-                "    verbose: true\n" +
-                "}";
+        String expected = "{\n" + "    host: hexlet.io\n" + "    timeout: 20\n" + "    verbose: true\n" + "}";
         String diff = Differ.generate(path1, path2);
 
         assertEquals(expected, diff);
@@ -27,11 +22,7 @@ class DifferTest {
         String path1 = "/Users/rasskazovaksenya/java-project-71/app/src/test/resources/test3.json";
         String path2 = "/Users/rasskazovaksenya/java-project-71/app/src/test/resources/test4.json";
 
-        String expected = "{\n" +
-                "    host: hexlet.io\n" +
-                "    verbose: true\n" +
-                "  + timeout: 20\n" +
-                "}";
+        String expected = "{\n" + "    host: hexlet.io\n" + "    verbose: true\n" + "  + timeout: 20\n" + "}";
         String diff = Differ.generate(path1, path2);
 
         assertEquals(expected, diff);
