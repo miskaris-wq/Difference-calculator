813d01aa7c73874f9837534fbbaf2d682befe2c9
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2025-03-12 17:51:31 +0300

    step 7

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -29,4 +29,38 @@ class DifferTest {
 
 
     }
+    @Test
+    void testGenerateDiffWhenFilesAreOneEmpty() throws Exception {
+        String path1 = "./src/test/resources/test5.json";
+        String path2 = "./src/test/resources/test6.json";
+
+        String expected = "{\n" + "  + host: hexlet.io\n" + "  + timeout: 20\n" + "  + verbose: true\n" + "}";
+        String diff = Differ.generate(path1, path2);
+
+        assertEquals(expected, diff);
+
+
+    }
+    @Test
+    void testGenerateDiffWhenFilesAreAllEmpty() throws Exception {
+        String path1 = "./src/test/resources/test5.json";
+        String path2 = "./src/test/resources/test8.json";
+
+        String expected = "{\n" + "}";
+        String diff = Differ.generate(path1, path2);
+
+        assertEquals(expected, diff);
+    }
+    @Test
+    void testGenerateDiffWhenFilesAreAnotherEmpty() throws Exception {
+        String path1 = "./src/test/resources/test6.json";
+        String path2 = "./src/test/resources/test5.json";
+
+        String expected = "{\n" + "  - host: hexlet.io\n" + "  - timeout: 20\n" + "  - verbose: true\n" + "}";
+        String diff = Differ.generate(path1, path2);
+
+        assertEquals(expected, diff);
+
+
+    }
 }
