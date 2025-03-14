5746135a4848dd18b33b0e51305dc6b6e5b1ffe2
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2025-03-14 16:06:13 +0300

    final step

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -1,39 +1,33 @@
 package hexlet.code.formatters;
 
 import java.util.Map;
+import java.util.TreeSet;
 
 public class PlainFormatter {
 
     public static String format(Map<String, Object> data1, Map<String, Object> data2) {
         StringBuilder result = new StringBuilder();
 
-        for (String key : data1.keySet()) {
+        TreeSet<String> allKeys = new TreeSet<>();
+        allKeys.addAll(data1.keySet());
+        allKeys.addAll(data2.keySet());
+
+        for (String key : allKeys) {
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
+
             if (!data2.containsKey(key)) {
                 result.append(String.format("Property '%s' was removed\n", key));
-            } else if (!isEqualValues(data1.get(key), data2.get(key))) {
-                result.append(String.format("Property '%s' was updated. From %s to %s\n",
-                        key, formatValue(data1.get(key)), formatValue(data2.get(key))));
-            }
-        }
-
-        for (String key : data2.keySet()) {
-            if (!data1.containsKey(key)) {
+            } else if (!data1.containsKey(key)) {
                 result.append(String.format("Property '%s' was added with value: %s\n",
                         key, formatValue(data2.get(key))));
+            } else if (!isEqualValues(value1, value2)) {
+                result.append(String.format("Property '%s' was updated. From %s to %s\n",
+                        key, formatValue(value1), formatValue(value2)));
             }
         }
 
         return result.toString().trim();
-    }
-
-    private static boolean isEqualValues(Object value1, Object value2) {
-        if (value1 == null && value2 == null) {
-            return true;
-        }
-        if (value1 == null || value2 == null) {
-            return false;
-        }
-        return value1.equals(value2);
     }
 
     private static String formatValue(Object value) {
@@ -44,5 +38,15 @@ public class PlainFormatter {
         } else {
             return String.valueOf(value);
         }
+    }
+
+    private static boolean isEqualValues(Object value1, Object value2) {
+        if (value1 == null && value2 == null) {
+            return true;
+        }
+        if (value1 == null || value2 == null) {
+            return false;
+        }
+        return value1.equals(value2);
     }
 }
