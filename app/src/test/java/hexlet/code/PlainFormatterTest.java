47540259310c898317ec6670a98e4269b4c946d1
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2025-03-14 16:32:40 +0300

    final step

--- app/src/test/java/hexlet/code/PlainFormatterTest.java
+++ app/src/test/java/hexlet/code/PlainFormatterTest.java
@@ -6,19 +6,19 @@ import java.util.TreeMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlainFormatterTest {
-    private  final int VALUE_FOR_KEY_2_1 = 42;
-    private  final int VALUE_FOR_KEY_2_2 = 100;
+    private  final int valueForKey21 = 42;
+    private  final int valueForKey22 = 100;
 
     @Test
     void testFormat() {
         Map<String, Object> data1 = new TreeMap<>();
         data1.put("key1", "value1");
-        data1.put("key2", VALUE_FOR_KEY_2_1);
+        data1.put("key2", valueForKey21);
         data1.put("key3", true);
 
         Map<String, Object> data2 = new TreeMap<>();
         data2.put("key1", "value1");
-        data2.put("key2", VALUE_FOR_KEY_2_2);
+        data2.put("key2", valueForKey22);
         data2.put("key4", "new value");
 
         String expected = """
